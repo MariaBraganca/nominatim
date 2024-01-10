0feaedbfdecfaa92c2ce0b35aa21db62495b644e
Maria Braganca <maria@chartmogul.com>
2024-01-10 22:37:46 +0100

    optimize postgres setup

--- postgresql/enable_postgis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# export DATABASE_USER="postgres"
-
-psql -v ON_ERROR_STOP=1 --username "$DATABASE_USER" --dbname "$DATABASE_USER" <<-EOSQL
-  CREATE EXTENSION postgis;
-  CREATE ROLE "www-data" LOGIN;
-EOSQL
